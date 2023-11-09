0118c9062aed5acb6442ce05900f0a20bb010aec
davidchang <daaniloveyou@gmail.com>
2023-11-09 15:20:47 +0800

    add form validation in edit invoice page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -64,13 +64,25 @@ export async function createInvoice(preState: State, formData: FormData) {
 // Use Zod to update the expected types
 const UpdateInvoice = InvoiceSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  preState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Invoice.",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
@@ -80,9 +92,7 @@ export async function updateInvoice(id: string, formData: FormData) {
     WHERE id = ${id}
   `;
   } catch (error) {
-    const message = "Database Error: Failed to Update Invoice.";
-    console.log(message);
-    throw new Error(message);
+    return { message: "Database Error: Failed to Update Invoice." };
   }
 
   revalidatePath("/dashboard/invoices");
